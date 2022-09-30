ad874540267004958ed9ab48e606268da1fe2d94
Bob Jackson <1187688895@qq.com>
2022-09-30 22:02:39 +0800

    update navbar

--- src/WebsiteNav.jsx
+++ src/WebsiteNav.jsx
@@ -2,23 +2,23 @@ import React from "react";
 import {Container, Nav, Navbar, NavDropdown} from "react-bootstrap";
 
 function WebsiteNav() {
-    return (<Navbar bg="light" expand="lg">
+    return (<Navbar bg="dark" expand="lg" variant="dark">
         <Container>
-            <Navbar.Brand href="#home">Programming-Books</Navbar.Brand>
+            <Navbar.Brand href="#home">Movies</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav"/>
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">
-                    <Nav.Link href="#home">Manning</Nav.Link>
-                    <Nav.Link href="#link">O'Reilly</Nav.Link>
-                    <NavDropdown title="Languages" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">Java</NavDropdown.Item>
+                    <Nav.Link href="#action">Action</Nav.Link>
+                    <Nav.Link href="#drama">Drama</Nav.Link>
+                    <Nav.Link href="#comedy">Comedy</Nav.Link>
+                    <Nav.Link href="#adventure">Adventure</Nav.Link>
+                    <Nav.Link href="#biography">Biography</Nav.Link>
+                    <Nav.Link href="#crime">Crime</Nav.Link>
+                    <Nav.Link href="#animation">Animation</Nav.Link>
+                    <NavDropdown title="Series" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="#action/3.1">Marvel</NavDropdown.Item>
                         <NavDropdown.Item href="#action/3.2">
-                            JavaScript
-                        </NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">Scala</NavDropdown.Item>
-                        <NavDropdown.Divider/>
-                        <NavDropdown.Item href="#action/3.4">
-                            C
+                            DC
                         </NavDropdown.Item>
                     </NavDropdown>
                 </Nav>
